38a3a6e586ae32fe616e5a393fc82269f3ce22dd
Irinaaissa <140831632+Irinaaissa@users.noreply.github.com>
2023-12-13 18:51:33 +0000

    Deploying to gh-pages from @ Irinaaissa/goit-js-hw-09@f4cc720f61b4f92af6c3fc0973d72cbb1866cc0e 🚀

--- commonHelpers3.js
+++ commonHelpers3.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";import{v as o}from"./assets/vendor-0f598f6d.js";console.log("Is mango@mail.com a valid email?: ",o.isEmail("mango@mail.com"));console.log("Is Mangodogmail.com a valid email?: ",o.isEmail("Mangodogmail.com"));
+import"./assets/modulepreload-polyfill-ec808ebb.js";import{v as o}from"./assets/vendor-01ca9d62.js";console.log("Is mango@mail.com a valid email?: ",o.isEmail("mango@mail.com"));console.log("Is Mangodogmail.com a valid email?: ",o.isEmail("Mangodogmail.com"));
 //# sourceMappingURL=commonHelpers3.js.map
