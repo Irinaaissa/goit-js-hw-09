905f7e961082647adb95407617f4650420fda6c6
Irinaaissa <irinaalisa7506@gmail.com>
2023-12-16 19:47:52 +0200

    2323

--- src/js/02-form.js
+++ src/js/02-form.js
@@ -1,44 +1,36 @@
-const form = document.querySelector('.feedback-form');
 
 
-  form.addEventListener('input', function (event) {
+  const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', function (event) {
     const formData = {
-      email: document.querySelector("[name='email']").value,
-      message: document.querySelector("[name='message']").value,
+        email: document.querySelector("[name='email']").value,
+        message: document.querySelector("[name='message']").value,
     };
-
     localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-  });
-
-localStorage.setItem('email', JSON.stringify(formData.email));
-localStorage.setItem('message', JSON.stringify(formData.message));
-
-document.addEventListener('DOMContentLoaded', function () {
-  if (localStorage.getItem('email') && localStorage.getItem('message')) {
-    document.querySelector('input[name="email"]').value = JSON.parse(
-      localStorage.getItem('email')
-    );
-    document.querySelector('textarea[name="message"]').value = JSON.parse(
-      localStorage.getItem('message')
-    );
-  }
+    localStorage.setItem('email', JSON.stringify(formData.email));
+    localStorage.setItem('message', JSON.stringify(formData.message));
 });
 
-document
-  .querySelector('.feedback-form')
-  .addEventListener('submit', function (event) {
+document.addEventListener('DOMContentLoaded', function () {
+    
+    const savedEmail = JSON.parse(localStorage.getItem('email'));
+    const savedMessage = JSON.parse(localStorage.getItem('message'));
+
+});
+
+form.addEventListener('submit', function (event) {
     event.preventDefault();
     let email = document.querySelector("[name='email']").value;
     let message = document.querySelector("[name='message']").value;
     if (!email || !message) {
-      event.preventDefault();
-      alert('Please fill in both fields!');
+        alert('Please fill in both fields!');
     } else {
-      console.log({ email, message });
-      localStorage.setItem('email', JSON.stringify(email));
-      localStorage.setItem('message', JSON.stringify(message));
+        console.log({ email, message });
+        localStorage.setItem('email', JSON.stringify(email));
+        localStorage.setItem('message', JSON.stringify(message));
     }
     document.querySelector("[name='email']").value = '';
     document.querySelector("[name='message']").value = '';
     event.target.reset();
-  });
+});
